7ca4f9b4756a42f12616093c458c2e4233cda197
Selena Mora <selenacardenaas1023@gmail.com>
2025-06-04 15:07:37 -0500

    primer commit agrego program.cs

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FigurasGeometricas
+{
+    public class Circulo
+    {
+        private double radio;
+
+        public Circulo(double radio)
+        {
+            this.radio = radio;
+        }
+
+        public double CalcularArea()
+        {
+            return Math.PI * radio * radio;
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * Math.PI * radio;
+        }
+    }
+
+    public class Rectangulo
+    {
+        private double baseRect;
+        private double altura;
+
+        public Rectangulo(double baseRect, double altura)
+        {
+            this.baseRect = baseRect;
+            this.altura = altura;
+        }
+
+        public double CalcularArea()
+        {
+            return baseRect * altura;
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * (baseRect + altura);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circulo = new Circulo(5);
+            Console.WriteLine("Área del círculo: " + circulo.CalcularArea());
+            Console.WriteLine("Perímetro del círculo: " + circulo.CalcularPerimetro());
+
+            Rectangulo rectangulo = new Rectangulo(4, 6);
+            Console.WriteLine("Área del rectángulo: " + rectangulo.CalcularArea());
+            Console.WriteLine("Perímetro del rectángulo: " + rectangulo.CalcularPerimetro());
+
+            Console.ReadLine(); // Para pausar la consola
+        }
+    }
+}
